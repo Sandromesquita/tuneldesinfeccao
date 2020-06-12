ede0736aa43bc94fce8c08d3b74792ca5fb30603
Sandro Mesquita <39969611+Sandromesquita@users.noreply.github.com>
2020-06-12 16:18:24 -0300

    Versão 2
    Nesta versão adicionamos 2 sensores de nível d'agua no tanque e 3 Leds para indicação visual destes níveis.
    Programado 3 estados do tunel: 
      Habilitado para funcionamento (tanque com água)
      Desabilitado para funcionamento (tanque sem água)
      Estado de erro (Falha nos sensores)

---
+++ borrifador-covid19-2.0.ino
@@ -0,0 +1,123 @@
+/*
+   PROJECT OF SPRAY AGAINST COVID19
+   Version: 2.0
+   Client: Pixels Escola de Design e Tecnologia
+   Start: 5/26/2020
+   Finish:
+   Update: 6/12/2020
+
+*/
+//Definition of the Arduino pins
+const byte pinInputSensor = 2; //Digital pin 2 of the Arduino connected to sense 1 (Thread blue)
+const byte pinMiddleSensor = 3; //Digital pin 3 of the Arduino connected to sense 2 (Thread yellow)
+const byte pinOutputSensor = 4; //Digital pin 4 of the Arduino connected to sense 3 (Thread orange)
+const byte pinRelayPump = 5; //Digital pin 5 of the Arduino connected to pin Relay of Pump Water
+const byte pinBuzzer = 6; //Digital pin 6 of the Arduino connected to buzzer (Thread green)
+const byte pinGreenLed = 8; // Digital pin 8 of the Arduino connected to Led Green
+const byte pinYellowLed = 9; // Digital pin 9 of the Arduino connected to Led Yellow
+const byte pinRedLed = 10; // Digital pin 10 of the Arduino connected to Led Red
+const byte pinMedLevelSensor = 12; // Digital pin 12 of the Arduino connected to Sensor of Medium Level (Thread white)
+const byte pinLowLevelSensor = 13; // Digital pin 11 of the Arduino connected to Sensor of Low Level (Thread orange)
+
+//Definition of variables of the sensors
+bool inputSensor, middleSensor, outputSensor; //Variables that keep the state of the of presence sensors
+bool lowLevelSensor, medLevelSensor; //Variables that keep the state of the of water level sensors
+
+void setup() {
+  Serial.begin(9600); //Starting serial communication
+  //Sets the Sensors pins
+  pinMode(pinInputSensor, INPUT); //Define the pin 2 as input
+  pinMode(pinMiddleSensor, INPUT); //Define the pin 3 as input
+  pinMode(pinOutputSensor, INPUT); //Define the pin 4 as input
+  pinMode(pinLowLevelSensor, INPUT_PULLUP); //Define the pin 11 as input
+  pinMode(pinMedLevelSensor, INPUT_PULLUP); //Define the pin 12 as input
+
+  //Sets the Actuators pins
+  pinMode(pinRelayPump, OUTPUT); //Define the pin 5 as output
+  pinMode(pinBuzzer, OUTPUT); //Define the pin 6 as output
+  pinMode(pinGreenLed, OUTPUT); //Define the pin 8 as output
+  pinMode(pinYellowLed, OUTPUT); //Define the pin 9 as output
+  pinMode(pinRedLed, OUTPUT); //Define the pin 10 as output
+
+}//End of the setup
+
+void loop() {
+  //Checks water level sensors
+  lowLevelSensor = digitalRead(pinLowLevelSensor);
+  medLevelSensor = digitalRead(pinMedLevelSensor);
+
+  //Logic of water level sensors: 0 --> With water | 1 --> No water
+
+  // If both sensors are active (With water)
+  if ((lowLevelSensor == 0) and (medLevelSensor == 0)) {
+    digitalWrite(pinGreenLed, HIGH);
+    digitalWrite(pinYellowLed, LOW);
+    digitalWrite(pinRedLed, LOW);
+    enabledTunnel();// Tunnel operating function
+  }
+
+  // If the medium level sensor not activated (Low water)
+  if ((lowLevelSensor == 0) and (medLevelSensor == 1)) {
+    digitalWrite(pinGreenLed, LOW);
+    digitalWrite(pinYellowLed, HIGH);
+    digitalWrite(pinRedLed, LOW);
+    enabledTunnel();// Tunnel operating function
+  }
+
+  // If the low level sensor not activated (no water) but medium level sensor are active
+  if ((lowLevelSensor == 1) and (medLevelSensor == 0)) {
+    //Error in sensors
+    digitalWrite(pinGreenLed, HIGH);
+    digitalWrite(pinYellowLed, HIGH);
+    digitalWrite(pinRedLed, HIGH);
+    Serial.println("Level sensor in ERROR!");
+    disableTunnel();
+  }
+
+  // If both sensors not activated (empty tank)
+  if ((lowLevelSensor == 1) and (medLevelSensor == 1)) {
+    digitalWrite(pinGreenLed, LOW);
+    digitalWrite(pinYellowLed, LOW);
+    digitalWrite(pinRedLed, HIGH);
+    disableTunnel();// Tunnel no operating function
+  }
+  Serial.println("======================================================");
+} //End of the loop
+
+void enabledTunnel() {
+  Serial.println("Tunnel enabled.");
+  
+  //Reading the status of the sensors
+  inputSensor = digitalRead(pinInputSensor);
+  middleSensor = digitalRead(pinMiddleSensor);
+  outputSensor = digitalRead(pinOutputSensor);
+
+  //Prints sensor readings on the serial monitor
+  Serial.println("Input Sensor: " + String(inputSensor) + " | Middle Sensor: " + String(middleSensor) + " | Output Sensor: " + String(outputSensor));
+
+  //If any of the 3 motion sensors is trigged
+  if ((inputSensor == HIGH) || (middleSensor == HIGH) || (outputSensor == HIGH)) {
+    Serial.println("Detected person presence in the tunel.");
+    digitalWrite(pinRelayPump, HIGH); //Start the pump
+    noTone(pinBuzzer);
+    tone(pinBuzzer, 440, 200);
+    delay(300);
+  }
+
+  else { //If you don't
+    Serial.println("No movement in the tunel.");
+    digitalWrite(pinRelayPump, LOW); //Turn off the pump
+    noTone(pinBuzzer); //Disable the buzzer
+    delay(300);
+  }
+
+}//end enabledTunnel
+
+void disableTunnel() {
+  Serial.println("Tunnel disabled.");
+  digitalWrite(pinRelayPump, LOW); //Turn off the pump
+  noTone(pinBuzzer); //Disable the buzzer
+  delay(500);
+  tone(pinBuzzer, 3000, 200);
+  delay(500);
+}
